05d129998b98a2ee3eaee2bd9f5e892629ed1981
Polina Belash <polinka.zhuravlinka@gmail.com>
2025-02-03 18:13:57 +0300

    update vite config

--- Kinopoisk-clone/vite.config.ts
+++ Kinopoisk-clone/vite.config.ts
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/Kinopoisk-clone",
+  base: "/Kinopoisk-clone/Kinopoisk-clone",
 });
